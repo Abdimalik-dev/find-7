4440ec1c87f6dc5acccc936494e721b51fac57a7
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-19 21:27:08 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Good Luck 💪🏾
+
+let findSeven = number => {
+    for (let i = 0; i < number.length; i++) {
+      if (number[i] === 7) {
+        return "Found";
+      }
+    }
+    return "Not Found";
+  }
+  
+  console.log(findSeven([1, 2, 3, 4, 5, 6,7]));
